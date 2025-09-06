39b363558f9e5c52393d31db8bc1db5b9fa0f130
daxp472 <daxpatel.cg@gmail.com>
2025-09-06 16:51:38 +0530

    Deployed Site added to the backend server

--- Frontend/src/api/api.ts
+++ Frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({ 
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://odooxnmit-2025.onrender.com",
+  baseURL: "https://odooxnmit-2025.onrender.com/api",
   headers: {
     'Content-Type': 'application/json'
   }
